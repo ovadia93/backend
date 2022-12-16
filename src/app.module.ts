569989c41af05c782b5d70e0fe020f55bb5f288e
Ovadia93 <you@example.com>
2022-12-16 12:47:35 +0000

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,43 +5,41 @@ import { ClientsController } from './clients/clients.controller';
 import { ClientsService } from './clients/clients.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Client } from './clients/clients.entity';
-import { Contact } from './contact/contact.entity';
 import { ContactController } from './contact/contact.controller';
 import { ContactService } from './contact/contact.service';
+import { Contact } from './contact/contact.entity';
 import { Task } from './tasks/tasks.entity';
 import { TasksController } from './tasks/tasks.controller';
 import { TasksService } from './tasks/tasks.service';
 
-
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 8889,
-      username: 'ben',
-      password: '123456',
-      database: 'full-stack',
-      entities: [
-          __dirname + '/**/*.entity{.ts,.js}',
-      ],
-      synchronize: true,
-      logging: true,
-  }),
-  TypeOrmModule.forFeature([Client, Contact, Task]),
-  ],
-  controllers: [
-    AppController,
-    ClientsController,
-    ContactController,
-    TasksController
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 8889,
+            username: 'ben',
+            password: '123456',
+            database: 'full-stack',
+            entities: [
+                __dirname + '/**/*.entity{.ts,.js}',
+            ],
+            synchronize: true,
+            logging: true,
+        }),
+        TypeOrmModule.forFeature([Client, Contact, Task]),
     ],
-
-  providers: [
-    AppService,
-    ClientsService,
-    ContactService,
-    TasksService
+    controllers: [
+        AppController,
+        ClientsController,
+        ContactController,
+        TasksController,
+    ],
+    providers: [
+        AppService,
+        ClientsService,
+        ContactService,
+        TasksService,
     ],
 })
-export class AppModule {}
+export class AppModule { }
